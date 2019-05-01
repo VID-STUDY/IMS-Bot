7d4b0f3e21e3a653bb4e28fe983de6a3a0686702
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-01 19:34:42 +0500

    Add Calls, TvChannels, Price files, ad campaigns models

--- application/core/models.py
+++ application/core/models.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin
 
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     username = db.Column(db.String(100))
@@ -12,7 +13,8 @@ class User(db.Model):
     company_name = db.Column(db.String)
 
 
-class UserAdmin(db.Model, UserMixin):
+class AdminUser(db.Model, UserMixin):
+    __tablename__ = 'admin_users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), index=True)
     password_hash = db.Column(db.String(120))
@@ -24,6 +26,41 @@ class UserAdmin(db.Model, UserMixin):
         return check_password_hash(self.password_hash, password)
 
 
+class Call(db.Model):
+    __tablename__ = 'calls'
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(15))
+    time = db.Column(db.String(50))
+
+
+class TVChannel(db.Model):
+    __tablename__ = 'tv_channels'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    price_files = db.relationship('PriceFile', lazy='dynamic')
+
+
+class PriceFile(db.Model):
+    __tablename__ = 'price_files'
+    id = db.Column(db.Integer, primary_key=True)
+    telegram_id = db.Column(db.String)
+    file_path = db.Column(db.String)
+    channel_id = db.Column(db.Integer, db.ForeignKey('tv_channels.id'))
+
+
+class AdCampaign(db.Model):
+    __tablename__ = 'ad_campaigns'
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(150))
+    target_audience = db.Column(db.String)
+    age_of_audience = db.Column(db.String)
+
+    class TargetAudiences:
+        MALE = 'male'
+        FEMALE = 'female'
+        MALE_AND_FEMALE = 'male_and_female'
+
+
 @login.user_loader
-def load_user(id):
-    return UserAdmin.query.get(int(id))
+def load_user(user_id):
+    return AdminUser.query.get(int(user_id))
