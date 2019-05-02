2594d29428da6d9cd42e56d5dd0520cb3bfaa7f0
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-02 15:31:36 +0500

    Add call service to management call orders

---
+++ application/core/callservice.py
@@ -0,0 +1,52 @@
+from application import db
+from application.core.models import Call
+from . import userservice
+
+
+def _get_current_user_call(user_id: int):
+    """
+    Return current not confirmed user's call
+    :param user_id: User's Telegram-ID
+    :return: void
+    """
+    user = userservice.get_user_by_id(user_id)
+    return user.calls.filter(Call.confirmed != True).first()
+
+
+def make_call_by_user(user_id: int):
+    """
+    Make a new call by user or set to null old call
+    :param user_id: User's Telegram-ID
+    :return: void
+    """
+    current_call = _get_current_user_call(user_id)
+    if not current_call:
+        user = userservice.get_user_by_id(user_id)
+        new_call = Call()
+        user.calls.append(new_call)
+        db.session.add(user)
+    db.session.commit()
+
+
+def set_call_phone_number(user_id: int, phone_number: str):
+    """
+    Set phone number to current user's order to call
+    :param user_id: User's Telegram-ID
+    :param phone_number: user's phone number
+    :return: void
+    """
+    current_call = _get_current_user_call(user_id)
+    current_call.phone_number = phone_number
+    db.session.commit()
+
+
+def set_call_time(user_id: int, time: str):
+    """
+    Set time to current user's order to call
+    :param user_id: User's Telegram-ID
+    :param time: Time, when the call must be made
+    :return: void
+    """
+    current_call = _get_current_user_call(user_id)
+    current_call.time = time
+    db.session.commit()
