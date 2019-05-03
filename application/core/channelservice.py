6a283cb022136d406174a20cec561982cecc5aeb
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-04 01:49:06 +0500

    CRUD for channels

--- application/core/channelservice.py
+++ application/core/channelservice.py
@@ -1,5 +1,8 @@
 from application import db
 from application.core.models import TVChannel, PriceFile
+from config import Config
+from application.utils import tools
+import os
 
 
 def get_all_channels() -> list:
@@ -47,6 +50,10 @@ def get_package_offers_by_channel(name: str) -> list:
 
 
 def get_channels_only_with_package_offers() -> list:
+    """
+    Return channels that have at least one package offer
+    :return: list of application.core.models.TvChannel
+    """
     tv_channels = TVChannel.query.all()
     sorted_channels = []
     for channel in tv_channels:
@@ -56,12 +63,96 @@ def get_channels_only_with_package_offers() -> list:
 
 
 def get_channels_only_with_price_files() -> list:
+    """
+    Return channels that have at least one price
+    :return: list of application.core.models.TvChannel
+    """
     tv_channels = TVChannel.query.all()
     sorted_channels = []
     for channel in tv_channels:
         if channel.price_files.filter(PriceFile.is_package == False).count() > 0:
             sorted_channels.append(channel)
     return sorted_channels
+
+
+def get_channel_by_id(channel_id: int) -> TVChannel:
+    """
+    Get channel by id or abort with 404
+    :param channel_id: Channel's ID
+    :return: application.core.models.TvChannel
+    """
+    return TVChannel.query.get_or_404(channel_id)
+
+
+def update_channel(channel_id: int, name=None, price_files=None, package_offers_files=None):
+    """
+    Update a channel. Remove old price and package offers files and recreate them with new
+    :param channel_id: Channel's ID
+    :param name: new channel's name
+    :param price_files: new channel's prices
+    :param package_offers_files: new channel's package offers
+    :return: void
+    """
+    channel = get_channel_by_id(channel_id)
+    if name:
+        channel.name = name
+    if price_files:
+        current_prices = channel.price_files.filter(PriceFile.is_package == False).all()
+        map(db.session.delete, current_prices)
+        for file in price_files:
+            file_path = os.path.join(Config.UPLOAD_DIRECTORY, file.filename)
+            tools.save_file(file, file_path, recreate=True)
+            new_price = PriceFile(file_path=file_path, is_package=False)
+            channel.price_files.append(new_price)
+            db.session.add(new_price)
+    if package_offers_files:
+        current_package_offers = channel.price_files.filter(PriceFile.is_package == True).all()
+        map(db.session.delete, current_package_offers)
+        for file in package_offers_files:
+            file_path = os.path.join(Config.UPLOAD_DIRECTORY, file.filename)
+            tools.save_file(file, file_path, recreate=True)
+            new_package_offer = PriceFile(file_path=file_path, is_package=True)
+            channel.price_files.append(new_package_offer)
+            db.session.add(new_package_offer)
+    db.session.commit()
+
+
+def create_channel(name: str, price_files=None, package_offers_files=None):
+    """
+    Create a new channel
+    :param name: Channel name
+    :param price_files: Paths of price files
+    :param package_offers_files: Paths of package offers files
+    :return: void
+    """
+    channel = TVChannel(name=name)
+    db.session.add(channel)
+    if price_files:
+        for file in price_files:
+            file_path = os.path.join(Config.UPLOAD_DIRECTORY, file.filename)
+            tools.save_file(file, file_path, recreate=True)
+            new_price = PriceFile(file_path=file_path, is_package=False)
+            channel.price_files.append(new_price)
+            db.session.add(new_price)
+    if package_offers_files:
+        for file in package_offers_files:
+            file_path = os.path.join(Config.UPLOAD_DIRECTORY, file.filename)
+            tools.save_file(file, file_path, recreate=True)
+            new_package_offer = PriceFile(file_path=file_path, is_package=True)
+            channel.price_files.add(new_package_offer)
+            db.session.add(new_package_offer)
+    db.session.commit()
+
+
+def remove_channel(channel_id: int):
+    """
+    Remove channel
+    :param channel_id: Channel's ID
+    :return: void
+    """
+    channel = TVChannel.query.get_or_404(channel_id)
+    db.session.delete(channel)
+    db.session.commit()
 
 
 class ChannelNotFound(Exception):
