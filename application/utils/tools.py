467894ef45722fd974f1527f8f53036ba8dcb882
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-04 01:49:26 +0500

    Work with files

--- application/utils/tools.py
+++ application/utils/tools.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from dateutil import tz
+import os
 
 
 def convert_utc_to_asia(utc_date: datetime):
@@ -8,3 +9,19 @@ def convert_utc_to_asia(utc_date: datetime):
     utc = utc_date.replace(tzinfo=from_zone)
     local = utc.astimezone(to_zone)
     return local
+
+
+def save_file(file, path, recreate=False):
+    """
+    Save file to path
+    :param file: File
+    :param path: Path
+    :param recreate: If file exists remove old version, save new version
+    :return: void
+    """
+    if os.path.exists(path):
+        if recreate:
+            os.remove(path)
+        else:
+            raise FileExistsError()
+    file.save(path)
