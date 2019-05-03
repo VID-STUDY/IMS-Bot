bc84bb3af8f68eb8f6d47768022b723271588e8e
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-03 22:10:09 +0500

    Add calls routes

---
+++ application/admin/calls.py
@@ -0,0 +1,18 @@
+from application.admin import bp
+from application.core import callservice
+from flask_login import login_required
+from flask import render_template, redirect, url_for
+
+
+@bp.route('/calls', methods=['GET', 'HEAD'])
+@login_required
+def calls():
+    confirmed_calls = callservice.get_all_confirmed_calls()
+    return render_template('admin/calls.html', calls=confirmed_calls, title="Заказы звонков")
+
+
+@bp.route('/call/<int:call_id>/delete', methods=['GET', 'HEAD'])
+@login_required
+def delete_call(call_id: int):
+    callservice.remove_call(call_id)
+    return redirect(url_for('admin.calls'))
