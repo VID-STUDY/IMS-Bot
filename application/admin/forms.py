5f055e9b629f8b3d2da481897d71de5a67114d1f
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-04 02:00:43 +0500

    Add FileAllowed validators

--- application/admin/forms.py
+++ application/admin/forms.py
@@ -1,13 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, MultipleFileField, ValidationError
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed
 from application.core.models import TVChannel
 
 
 class ChannelForm(FlaskForm):
     name = StringField('Название канала', validators=[DataRequired('Имя обзательно')])
-    price_files = MultipleFileField('Файлы прайсов')
-    package_offers_files = MultipleFileField('Файлы пакетных предложений')
+    price_files = MultipleFileField('Файлы прайсов',
+                                    validators=[FileAllowed(['jpg', 'png', 'pdf', 'doc', 'docx'],
+                                                            message="Только файлы изображений или "
+                                                                    "документов")])
+    package_offers_files = MultipleFileField('Файлы пакетных предложений',
+                                             validators=[FileAllowed(['jpg', 'png', 'pdf', 'doc', 'docx'],
+                                                                     message="Только файлы изображений или "
+                                                                             "документов")])
     submit = SubmitField('Сохранить')
 
     def validate_name(self, name: StringField):
