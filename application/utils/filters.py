b58a262bd9054bd7cabfe990a9fa74a2fbfcc299
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-04 23:20:21 +0500

    Add flag to convert from utc

--- application/utils/filters.py
+++ application/utils/filters.py
@@ -3,5 +3,9 @@ from .tools import convert_utc_to_asia
 
 
 @app.template_filter
-def datetime(value, date_format='%d.%m.%Y %H:%M:%S'):
-    return convert_utc_to_asia(value).strftime(date_format)
+def datetime(value, date_format='%d.%m.%Y %H:%M:%S', convert_from_utc=False):
+    if convert_from_utc:
+        date = value
+    else:
+        date = convert_utc_to_asia(value)
+    return date.strftime(date_format)
