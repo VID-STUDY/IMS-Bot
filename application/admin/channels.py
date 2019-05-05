62069bc54c2802cc51f3a9c60c6ca5302a2f68c5
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-05 13:52:23 +0500

    Fix routes names

--- application/admin/channels.py
+++ application/admin/channels.py
@@ -25,7 +25,7 @@ def concrete_channel(channel_id: int):
         name = channel_form.name.data
         channelservice.update_channel(channel_id, name, price_files, package_offers_files)
         flash('Канал {} успешно изменён'.format(channel_form.name.data), category='success')
-        return redirect(url_for('admin.channels'))
+        return redirect(url_for('admin.all_channels'))
     channel = channelservice.get_channel_by_id(channel_id)
     channel_form.name.data = channel.name
     return render_template('admin/channel.html',
@@ -42,10 +42,13 @@ def new_channel():
     if channel_form.validate_on_submit():
         price_files = request.files.getlist('price_files')
         package_offers_files = request.files.getlist('package_offers_files')
+        if price_files or package_offers_files:
+            flash('Рекомендуется перейти в бота и запросить у него новые файлы, '
+                  'чтобы в последующем он отправлял их быстрее', category='warning')
         name = channel_form.name.data
         channelservice.create_channel(name, price_files, package_offers_files)
         flash('Канал {} успешно создан!'.format(channel_form.name.data), category='success')
-        return redirect(url_for('admin.channels'))
+        return redirect(url_for('admin.all_channels'))
     return render_template('admin/new_channel.html', title='Новый канал', form=channel_form, area='channels')
 
 
@@ -54,4 +57,4 @@ def new_channel():
 def delete_channel(channel_id: int):
     channelservice.remove_channel(channel_id)
     flash('Канал удалён', category='success')
-    return redirect(url_for('admin.channels'))
+    return redirect(url_for('admin.all_channels'))
