b17bd1572e141cedebabcfcb1115e4620993ca34
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-05 13:52:02 +0500

    Check filenames before saving

--- application/core/channelservice.py
+++ application/core/channelservice.py
@@ -101,6 +101,8 @@ def update_channel(channel_id: int, name=None, price_files=None, package_offers_
         current_prices = channel.price_files.filter(PriceFile.is_package == False).all()
         map(db.session.delete, current_prices)
         for file in price_files:
+            if file.filename == '':
+                continue
             file_path = os.path.join(Config.UPLOAD_DIRECTORY, secure_filename(file.filename))
             tools.save_file(file, file_path, recreate=True)
             new_price = PriceFile(file_path=file_path, is_package=False)
@@ -110,6 +112,8 @@ def update_channel(channel_id: int, name=None, price_files=None, package_offers_
         current_package_offers = channel.price_files.filter(PriceFile.is_package == True).all()
         map(db.session.delete, current_package_offers)
         for file in package_offers_files:
+            if file.filename == '':
+                continue
             file_path = os.path.join(Config.UPLOAD_DIRECTORY, secure_filename(file.filename))
             tools.save_file(file, file_path, recreate=True)
             new_package_offer = PriceFile(file_path=file_path, is_package=True)
@@ -130,6 +134,8 @@ def create_channel(name: str, price_files=None, package_offers_files=None):
     db.session.add(channel)
     if price_files:
         for file in price_files:
+            if file.filename == '':
+                continue
             file_path = os.path.join(Config.UPLOAD_DIRECTORY, secure_filename(file.filename))
             tools.save_file(file, file_path, recreate=True)
             new_price = PriceFile(file_path=file_path, is_package=False)
@@ -137,6 +143,8 @@ def create_channel(name: str, price_files=None, package_offers_files=None):
             db.session.add(new_price)
     if package_offers_files:
         for file in package_offers_files:
+            if file.filename == '':
+                continue
             file_path = os.path.join(Config.UPLOAD_DIRECTORY, secure_filename(file.filename))
             tools.save_file(file, file_path, recreate=True)
             new_package_offer = PriceFile(file_path=file_path, is_package=True)
