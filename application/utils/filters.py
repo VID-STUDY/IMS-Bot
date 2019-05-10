db307ec3b8a62c25753693ea503fd6043a1c7c87
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-11 02:40:03 +0500

    Views and filters for advertise order

--- application/utils/filters.py
+++ application/utils/filters.py
@@ -1,4 +1,5 @@
 from application import app
+from application.resources.strings import from_target_audience_enum_to_text, format_ages, budget_enum_to_text
 from .tools import convert_utc_to_asia
 
 
@@ -9,3 +10,19 @@ def datetime(value, date_format='%d.%m.%Y %H:%M:%S', convert_from_utc=False):
     else:
         date = value
     return date.strftime(date_format)
+
+
+@app.template_filter()
+def target_audience(value):
+    return from_target_audience_enum_to_text(value, 'ru')
+
+
+@app.template_filter()
+def audience_age(value):
+    return format_ages(value, 'ru')
+
+
+
+@app.template_filter()
+def budget(value):
+    return budget_enum_to_text(value, 'ru')
